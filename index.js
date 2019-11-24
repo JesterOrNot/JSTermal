ed680f652d8404e48e0b500a118d4f89a7426444
Sean Hellum <seanhellum45@gmail.com>
2019-11-24 00:47:44 +0000

    add command not found

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var colors = require('colors');
 while (true) {
     input = readline.question("js-shell> ".green);
     if (input === "help") {
-        console.log("commands".underline + ":\n" + "help".underline + ": display this help message!");
+        console.log("commands".underline.yellow + ":\n" + "help".underline.yellow + ": display this help message!");
+    } else {
+        console.log("js-shell: ".red + input.red + ": command not found".red)
     }
 }
