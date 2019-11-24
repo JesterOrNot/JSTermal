9fb961b74af8624898c9ca8136e09dc3c0f18ba0
Sean Hellum <seanhellum45@gmail.com>
2019-11-24 00:44:17 +0000

    underline stuff for help command

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var colors = require('colors');
 while (true) {
     input = readline.question("js-shell> ".green);
     if (input === "help") {
-        console.log("commands:\nhelp: display this help message!");
+        console.log("commands".underline + ":\n" + "help".underline + ": display this help message!");
     }
 }
