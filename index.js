5193e43493e4544d4974a832b73d897a5ba433ef
Sean Hellum <seanhellum45@gmail.com>
2019-11-24 00:40:46 +0000

    color output

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var readline = require('readline-sync');
+var colors = require('colors');
+
 while (true) {
-    input = readline.question("js-shell> ");
+    input = readline.question("js-shell> ".green);
     if (input === "help") {
         console.log("commands:\nhelp: display this help message!");
     }
