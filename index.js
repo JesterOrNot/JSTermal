b97b4d868e8ca8cb38bf1e4240f9b0ef4d623164
Sean Hellum <seanhellum45@gmail.com>
2019-11-24 00:55:19 +0000

    misc updates

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var colors = require('colors');
 while (true) {
     input = readline.question("js-shell> ".green);
     if (input === "help") {
-        console.log("commands".underline.yellow + ":\n" + "help".underline.yellow + ": display this help message!");
+        console.log("commands".underline.yellow + ":\n" + "help".underline.yellow + ": display this help message!".rainbow);
+    } else if (input.length === 0) {
+        continue;
     } else {
-        console.log("js-shell: ".red + input.red + ": command not found".red)
+        console.log("js-shell: ".red.italic.bold + input.red.italic.bold + ": command not found!".red.italic.bold);
     }
 }
